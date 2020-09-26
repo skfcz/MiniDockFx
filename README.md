c540ca49fb0c517f55dd2facfef51454bef3d552
Zerbst, Carsten <Carsten.Zerbst@PROSTEP.com>
2020-09-26 20:19:12 +0200

    added copyright headers * updated name in license * add screen shot * write documentation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # MiniDockFx
+
+This package provides (yet another) simple docking component for JavaFX.
+Most alternatives in 2020 did either not run on all operating systems or were way too complex. 
+
+![image](data/screen.png)
+
+This docking component provides the following capabilities:
+
+* has a fixed layout with 4 docking areas (left, center,right, bottom)
+* each docking area is a JFXTabbedPane
+* users could move the tabs by dragging them onto a small screen element showing possible docking areas
+* users could close the tabs
+* split positions are stored in java.util.Preferences  
+
+The implementation is pretty simple, it is just four classes and two fxml files.
+Styling is possible using CSS, there are no hardcoded values.
+
+## Usage
+
+1. Derive a view from AbstractTabableView. Here you could provided
+   * the view name displayed in the tab
+   * change whether your views supports moving or closing by a user
+   * the Node you want to display
+2. Instantiate a MiniDockFXPane
+3. Add the views to the dock pane
+
+See [Main.java](src/test/java/de/cadoculus/javafx/minidockfx/demo/Main.java) as example how to use the docking area,
+[ExampleTabview1](src/test/java/de/cadoculus/javafx/minidockfx/demo/ExampleTabview1.java) as example how to implement a view.
+
+## Dependencies
+* javafx
+* jfoenix
+* ikonli with fontawesome pack
+* slf4j
+
+## License
+
+This code is under a  [BSD License](LICENSE), so feel free to use it without too many obligations :-)
+    
--- src/main/java/de/cadoculus/javafx/minidockfx/AbstractTabableView.java
+++ src/main/java/de/cadoculus/javafx/minidockfx/AbstractTabableView.java
@@ -1,3 +1,34 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020, Carsten Zerbst
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.cadoculus.javafx.minidockfx;
 
 import javafx.beans.property.BooleanProperty;
@@ -26,8 +57,9 @@ public abstract class AbstractTabableView {
 
     /**
      * The default constructor
+     *
      * @param name     the initial view name. Use {@link #name} to change it at runtime
-     * @param id the id used to store position in preferences. No storage used if id is empty or null.
+     * @param id       the id used to store position in preferences. No storage used if id is empty or null.
      * @param canClose true if the view is closeable. If false no button is added to the tab.
      * @param canMove  true if the view is moveable. If false it is not possible to move the tab
      */
